d98c65f28cba6b1452dc279cfa2e7da839193a76
staruniverse2050 <105754123+staruniverse2050@users.noreply.github.com>
2022-06-17 19:06:07 -0500

    Update Orender.php

--- Orender.php
+++ Orender.php
@@ -1,92 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
 
-        <!DOCTYPE html>
-        <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="imagenes/user.png">
+    <title>Iniciar Sesion</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Orender</title>
-            <link rel="icon" type="image/png" href="img/inicio.ico">
-            <link rel="stylesheet" href="css/estilos.css">
-            <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-            <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
-            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+</head>
 
+<body>
+    <div class="login">
+        <form action="conexion.php" method="POST" class="form" class="form">
+            <h1 class="title">Iniciar Sesión</h1>
+            <div class="inputcontainer">
+                <input type="text" class="input" name="Email" placeholder="Correo Electronico">
+                <label for="" class="label">Correo Electronico</label>
+            </div>
+            <div class="inputcontainer">
+                <input type="password" class="input" name="contraseña" placeholder=" Contraseña ">
+                <label for=" " class="label ">Contraseña</label>
+            </div>
+            <input type="submit" class="submit" name="IniciarSesion" value="Ingresar">
+        </form>
+    </div>
+</body>
 
-
-        </head>
-
-        <body>
-            <header class="encabezado">
-                <nav>
-                    <ul>
-                        <li>
-                            <div class="Sesion">
-                                <a href="#">Cerrar Sesión</A>
-                    </ul>
-        
-                </nav>
-                <div class="imagen">
-                <img src="img/orender.png">
-            </header>
-            <header class="header" id="Menu">
-                <div class="contenedor head">
-                    <h1 class="titulo">Hacemos tu diseño ideal en una realidad</h1>
-                    <p class="text">Tú imaginas y nosotros nos ponemos en marcha de hacerlo realidad.</p>
-                </div>
-            </header>
-
-                    <main class="cuadro">
-                        <a href="./" class="social-media-iconico">
-                            <i class='bx bxs-hard-hat'></i>
-                            <h5>Servicios</h5>
-                        </a>
-
-                        <a href="Nosotros/nosotros.html" class="social-media-iconico">
-                            <i class='bx bxs-book-reader'></i>
-                            <h5>Nosotros</h5>
-                        </a>
-
-                        <a href="./" class="social-media-iconico">
-                            <i class='bx bxs-folder-open'></i>
-                            <h5>Maquetado</h5>
-                        </a>
-
-                        <a href="Galeria/galeria.html" class="social-media-iconico ">
-                            <i class='bx bx-images'></i>
-                            <h5 class="letter">Galeria</h5>
-                        </a>
-
-
-
-                </div>
-                </main>
-            </section>
-            <div class="pie ">
-                <footer id="contacto ">
-                    <div class="contenedor footer-content ">
-                        <div class="contact-us ">
-                            <h2 class="brand ">ORENDER</h2>
-                            <p>ADSI (Análisis y sistemas de información)</p>
-                            <p>Centro de comercio y turismo</p>
-                            <p>S E N A</p>
-                        </div>
-                        <div class="social-media ">
-                            <a href="https://orender3d.com/#inicio " class="social-media-icon ">
-                                <i class='bx bx-world'></i>
-                            </a>
-
-
-                        </div>
-                    </div>
-
-                </footer>
-
-        </body>
-
-        </html>
+</html>
